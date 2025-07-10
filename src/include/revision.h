6998f795ecf2c67ce68436b697b49f03e280969f
mellw0101 <melwivensson@gmail.com>
2025-07-10 21:04:25 +0200

    No msg

--- src/include/revision.h
+++ src/include/revision.h
@@ -1,6 +1,6 @@
 #define REVISION  "v0.0.1"
 
-#define VERSION_TEXT  \
+#define VERSION_TEXT                                                                         \
   "  'NanoX %s' is a fork of `GNU nano v8.0-44-gef1c9b9f` from git source code."       "\n"  \
   "  First converted from 'C' into 'C++', and modernized to include mush more"         "\n"  \
   "  modern features, sush as (whole line moving / selection region moving),"          "\n"  \
