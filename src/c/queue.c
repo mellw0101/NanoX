251eaa7918a5634299dc166faf7491ebe3269066
mellw0101 <melwivensson@gmail.com>
2025-12-18 16:48:23 +0100

    No msg

---
+++ src/c/queue.c
@@ -0,0 +1,8 @@
+#include "../include/c_proto.h"
+#include "atomic.inc"
+
+
+/* ---------------------------------------------------------- Typedef's ---------------------------------------------------------- */
+
+
+typedef struct QUEUE_T *  QUEUE;
